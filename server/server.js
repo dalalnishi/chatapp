0a94733eb56e9e321dca2f03f79a566b78627d3e
lcom137-one <dalalnishi96@gmail.com>
2018-12-19 14:06:14 +0530

    chat app3

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const exp=require('express');
+
+const socketIO=require('socket.io');
+const http=require('http');
+
+const path=require('path');
+const pub=path.join(__dirname,'../public');
+
+const port=process.env.PORT || 5000;
+var app=exp();
+app.use(exp.static(pub));
+
+var server=http.createServer(app);
+var io=socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('User connected');
+    
+    socket.on('createMessage',(newMsg)=>{
+        console.log('createMessage',newMsg);
+        
+        io.emit('newMessage',{
+            from:newMsg.from,
+            text:newMsg.text,
+            createdAt:new Date().getTime()
+        });           
+    });
+    
+    socket.on('disconnect',()=>{
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port,()=>{
+    console.log(`Started on port ${port}`);
+});
